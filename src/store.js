4883fb781000a3e8c662ea111613a4d21dcfeea3
Slartbartfast1 <38152942+Slartbartfast1@users.noreply.github.com>
2018-10-01 19:58:19 +0800

    store

--- src/store.js
+++ src/store.js
@@ -5,14 +5,28 @@ import createPersistedState from 'vuex-persistedstate'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-
-  },
-  mutations: {
-
-  },
-  actions: {
-
-  },
+    state: {
+        list: [{category: '数码产品', payout: 20.00, desc: 'sadas', date: '2017-12-12 12:12:12'}, {
+            category: '教育',
+            payout: 20.00,
+            desc: 'sadas',
+            date: '2017-12-12 12:12:12'
+        },],
+    },
+    mutations: {
+        add(state, newPayout) {
+            state.list.push(newPayout)
+        },
+        remove(state, obj) {
+            var target=state.list.filter(function(el){
+                if(el.id===obj){
+                    return true
+                }
+            });
+            var i=state.list.indexOf(target[0]);
+            state.list.splice(i, 1)
+        }
+    },
+    actions: {},
     plugins: [createPersistedState()],
 })
